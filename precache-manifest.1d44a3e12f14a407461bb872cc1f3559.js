22aee0d75c4773ddcf1de9ceabd275b02a491803
David Pfahler <pfahler.david@gmail.com>
2020-09-29 15:50:38 +0200

    Updates

---
+++ precache-manifest.1d44a3e12f14a407461bb872cc1f3559.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b878da8dbde529254657a5338afc179",
+    "url": "./index.html"
+  },
+  {
+    "revision": "82068d0652ad12d9b147",
+    "url": "./static/js/2.53fc0393.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.53fc0393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd4ee8948a6feee7031",
+    "url": "./static/js/main.5135b8a7.chunk.js"
+  },
+  {
+    "revision": "6b34eae992cad69f8222",
+    "url": "./static/js/runtime-main.03dfff44.js"
+  }
+]);
